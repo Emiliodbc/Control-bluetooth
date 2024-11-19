7f422787d708e83c57b0e8dcd2599841c64834c5
Emiliodbc <a01067698@tec.mx>
2024-11-19 08:27:20 -0600

    bt Control_boton_servo.ino

--- Control_boton_servo.ino
+++ Control_boton_servo.ino
@@ -34,48 +34,46 @@ void setup() {
 }
 
 void loop() {
-  // Leer el estado de los botones para servo 1
-  bool boton_1_1_estado = digitalRead(Boton_1_1);
-  bool boton_1_2_estado = digitalRead(Boton_1_2);
-
-  // Si el botón 1_1 está presionado, mover servo 1 a la derecha
-  if (boton_1_1_estado == HIGH) {
-    if (servo1_pos < 180) {
-      servo1_pos++;  // Mueve el servo 1 hacia la derecha
-      myservo1.write(servo1_pos);
-      delay(10);  // Retardo para evitar movimientos bruscos
+ 
+  if (Serial.available()){
+    comd = Serial.readString();  //read until timeout
+    comd.trim();                        // remove any \r \n whitespace at the end of the String
+    Serial.println(comd);
+          
+    // Si el botón 1_1 está presionado, mover servo 1 a la derecha
+    if (comd=="A") {
+      if (servo1_pos < 180) {
+        servo1_pos++;  // Mueve el servo 1 hacia la derecha
+        myservo1.write(servo1_pos);
+        delay(10);  // Retardo para evitar movimientos bruscos
+      }
     }
-  }
-
-  // Si el botón 1_2 está presionado, mover servo 1 a la izquierda
-  if (boton_1_2_estado == HIGH) {
-    if (servo1_pos > 0) {
-      servo1_pos--;  // Mueve el servo 1 hacia la izquierda
-      myservo1.write(servo1_pos);
-      delay(10);  // Retardo para evitar movimientos bruscos
+  
+    // Si el botón 1_2 está presionado, mover servo 1 a la izquierda
+    if (comd=="B") {
+      if (servo1_pos > 0) {
+        servo1_pos--;  // Mueve el servo 1 hacia la izquierda
+        myservo1.write(servo1_pos);
+        delay(10);  // Retardo para evitar movimientos bruscos
+      }
     }
-  }
-
-  // Leer el estado de los botones para servo 2
-  bool boton_2_1_estado = digitalRead(Boton_2_1);
-  bool boton_2_2_estado = digitalRead(Boton_2_2);
-
-  // Si el botón 2_1 está presionado, mover servo 2 a la derecha
-  if (boton_2_1_estado == HIGH) {
-    if (servo2_pos < 180) {
-      servo2_pos++;  // Mueve el servo 2 hacia la derecha
-      myservo2.write(servo2_pos);
-      delay(10);  // Retardo para evitar movimientos bruscos
+  
+    // Si el botón 2_1 está presionado, mover servo 2 a la derecha
+    if (comd=="C") {
+      if (servo2_pos < 180) {
+        servo2_pos++;  // Mueve el servo 2 hacia la derecha
+        myservo2.write(servo2_pos);
+        delay(10);  // Retardo para evitar movimientos bruscos
+      }
     }
-  }
-
-  // Si el botón 2_2 está presionado, mover servo 2 a la izquierda
-  if (boton_2_2_estado == HIGH) {
-    if (servo2_pos > 0) {
-      servo2_pos--;  // Mueve el servo 2 hacia la izquierda
-      myservo2.write(servo2_pos);
-      delay(10);  // Retardo para evitar movimientos bruscos
+  
+    // Si el botón 2_2 está presionado, mover servo 2 a la izquierda
+    if (comd=="D") {
+      if (servo2_pos > 0) {
+        servo2_pos--;  // Mueve el servo 2 hacia la izquierda
+        myservo2.write(servo2_pos);
+        delay(10);  // Retardo para evitar movimientos bruscos
+      }
     }
   }
 }
-
