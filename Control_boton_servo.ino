bff8bffa6daa949d8215724ab53651b563003963
Emiliodbc <a01067698@tec.mx>
2024-11-23 15:44:56 -0600

    Update Control_boton_servo.ino

--- Control_boton_servo.ino
+++ Control_boton_servo.ino
@@ -34,6 +34,7 @@ void loop() {
     // cambiar entre manejar y garra 
     
   if (comd=="A"){
+    Serial.print("detecto comando A");
     if (mode==0){
       mode=1;
     } else {
@@ -44,6 +45,7 @@ void loop() {
   if (mode==0){
        // Si el botón 1_1 está presionado, mover servo 1 a la derecha
       if (comd=="B") {
+        Serial.print("detecto comando B");
         if (servo1_pos < 180) {
           servo1_pos++;  // Mueve el servo 1 hacia la derecha
           myservo1.write(servo1_pos);
@@ -53,6 +55,7 @@ void loop() {
     
       // Si el botón 1_2 está presionado, mover servo 1 a la izquierda
       if (comd=="C") {
+        Serial.print("detecto comando C");
         if (servo1_pos > 0) {
           servo1_pos--;  // Mueve el servo 1 hacia la izquierda
           myservo1.write(servo1_pos);
@@ -62,6 +65,7 @@ void loop() {
     
       // Si el botón 2_1 está presionado, mover servo 2 a la derecha
       if (comd=="D") {
+        Serial.print("detecto comando D");
         if (servo2_pos < 180) {
           servo2_pos++;  // Mueve el servo 2 hacia la derecha
           myservo2.write(servo2_pos);
@@ -71,6 +75,7 @@ void loop() {
     
       // Si el botón 2_2 está presionado, mover servo 2 a la izquierda
       if (comd=="E") {
+        Serial.print("detecto comando E");
         if (servo2_pos > 0) {
           servo2_pos--;  // Mueve el servo 2 hacia la izquierda
           myservo2.write(servo2_pos);
@@ -79,10 +84,11 @@ void loop() {
       }
       //TOGGLE CLOSE OR OPEN CLAW
       if(comd=="F"){
+        Serial.print("detecto comando F");
         if (garaOC==0){
-         while(servo_3pos<180){
+         while(servo3_pos<180){
             servo3_pos++;  // Mueve el servo 3 hacia la derecha
-            myservo3.write(servo3_pos);
+            myservo3.write(servo_3pos);
             delay(10);  // Retardo para evitar movimientos bruscos
           }
           mode=1;
@@ -133,3 +139,4 @@ void loop() {
       }
     }
   }
+}
